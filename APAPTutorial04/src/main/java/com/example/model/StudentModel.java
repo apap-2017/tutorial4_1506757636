6ee43bf312d5ed529773a62fbf391727eff0efd9
alham <alhamfebian68@gmail.com>
2017-09-30 23:06:24 +0700

    tutorial4

---
+++ APAPTutorial04/src/main/java/com/example/model/StudentModel.java
@@ -0,0 +1,40 @@
+package com.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentModel
+{
+    private String npm;
+    private String name;
+    private double gpa;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+}
